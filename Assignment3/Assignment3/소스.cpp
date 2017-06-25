2af611ed3c6b5d6a0242a29a903524971cef8bb9
sg201324423 <sgkim5955@gmail.com>
2017-06-25 18:53:30 +0900

    remove flaw

--- Assignment3/Assignment3/소스.cpp
+++ Assignment3/Assignment3/소스.cpp
@@ -343,13 +343,12 @@ int numOfProfessorRecords = BLOCKSIZE / sizeof(Professors);
 
 
 
-Node * root;
-Node * root2;
+Node * studentRoot;
+Node * professorRoot;
 Node * Stack[MAX];
 int StackPoint;
 
-void initBPlusTree();
-void initBPlusTree2();
+void initBPlusTree(Node*& node);
 
 void initStack();
 Node * getStack();
@@ -534,7 +533,7 @@ int getProfessorData(ProfessorBlock*& blocks, string input_str) {
 int writeStudentIndexFile(FILE *& fout)
 {
 	fseek(fout, 0, SEEK_SET);
-	Node * p = root;
+	Node * p = studentRoot;
 
 	if (p->type == INDEX)
 		for (; p->node.indexNode.pointer[0] != NULL; p = p->node.indexNode.pointer[0]);
@@ -554,7 +553,7 @@ int writeStudentIndexFile(FILE *& fout)
 int writeProfessorIndexFile(FILE *& fout)
 {
 	fseek(fout, 0, SEEK_SET);
-	Node * p = root2;
+	Node * p = professorRoot;
 
 	if (p->type == INDEX)
 		for (; p->node.indexNode.pointer[0] != NULL; p = p->node.indexNode.pointer[0]);
@@ -664,8 +663,8 @@ int getQueryData(string*& querySet) {
 int main()
 {
 	int num;
-	initBPlusTree();
-	initBPlusTree2();
+	initBPlusTree(studentRoot);
+	initBPlusTree(professorRoot);
 	initStack();
 
 	studentCnt = getStudentData(writeStudentBlocks, "student_data.csv");
@@ -734,7 +733,7 @@ int main()
 	//insert <key, value> into index
 	for (int j = 0; j < studentCnt / numOfStudentRecords + 1; j++) {
 		for (int i = 0; i < numOfStudentRecords; i++) {
-			InsertKey(readStudentBlocks[j].records[i].score, j, root);
+			InsertKey(readStudentBlocks[j].records[i].score, j, studentRoot);
 		}
 	}
 
@@ -744,7 +743,7 @@ int main()
 	//insert <key, value> into index
 	for (int j = 0; j < professorCnt / numOfProfessorRecords + 2; j++) {
 		for (int i = 0; i < numOfProfessorRecords; i++) {
-			InsertKey(readProfessorBlocks[j].records[i].salary, j, root2);
+			InsertKey(readProfessorBlocks[j].records[i].salary, j, professorRoot);
 		}
 	}
 
@@ -766,7 +765,7 @@ int main()
 	HashMap* readProfessorHashMap = new HashMap[professorCnt];
 	Node* readStudentScoreIdx = new Node[DEGREE];
 	Node* readProfessorSalaryIdx = new Node[DEGREE];
-
+/*
 	
 	while (1) {
 		cout << "Select your operation\n 1.Show Students.hash\n 2.Show all the leaves of Students_score.idx\n 3.Show Stuents.DB\n"
@@ -808,7 +807,7 @@ int main()
 				}
 			}
 			break;
-		case 7:
+		case 7:*/
 			for (int i = 0; i < queryCnt; i++) {
 				tokenizer.setString(querySet[i]);
 				if (tokenizer.next() == "Search-Exact") {
@@ -825,17 +824,17 @@ int main()
 					string type = tokenizer.next();
 					tokenizer.next();
 					if (type == "Students") {
-						rangeSearch(root, atoi(tokenizer.next().c_str()), atoi(tokenizer.next().c_str()), "Students");
+						rangeSearch(studentRoot, atoi(tokenizer.next().c_str()), atoi(tokenizer.next().c_str()), "Students");
 					}
 					else if (type == "Professors") {
-						rangeSearch(root2, atoi(tokenizer.next().c_str()), atoi(tokenizer.next().c_str()), "Professors");
+						rangeSearch(professorRoot, atoi(tokenizer.next().c_str()), atoi(tokenizer.next().c_str()), "Professors");
 					}
 				}
 				else if (tokenizer.next() == "Join") {
 
 				}
 			}
-			break;
+		/*	break;
 		case 8:
 			return 0;
 			break;
@@ -845,7 +844,7 @@ int main()
 
 		cout << endl;
 	}
-
+	*/
 	system("pause");
 	return 0;
 }
@@ -897,7 +896,7 @@ void SelectKey(float selectKey)
 {
 	Node *thisNode;
 	int i;
-	if ((thisNode = FindKey(selectKey, 1, root)) == NULL)
+	if ((thisNode = FindKey(selectKey, 1, studentRoot)) == NULL)
 	{
 		printf("Key = %.1f\n", selectKey);
 		puts("키가 존재하지 않습니다.\n");
@@ -1162,326 +1161,7 @@ void initStack()
 		Stack[i] = NULL;
 	StackPoint = 0;
 }
-void DeleteKey2(Node *thisNode)
-{
-	/*
-	DeleteKey2 함수
-	인덱스 노드의 재분배 및 병합하는 함수
-
-	Node *thisNode : 재 분할 및 병합을 해야하는 노드
-	*/
-	int i, j, k;
-	int thisPoint;
-	Node *parent;
-	Node *broNode;
-	Node *tempNode;
-	// 현재 노드가 인덱스 노드가 아니라면 종료
-	if (thisNode->type != INDEX) return;
-	// 부모노드 설정
-	parent = thisNode->parent;
-	// 현재 노드가 루트이면 종료
-	if (parent == NULL) return;
-	// 현재 노드가 부모 노드의 몇번째 서브트리 인지 확인
-	for (thisPoint = 0; thisPoint < DEGREE - 1; thisPoint++)
-		if (parent->node.indexNode.pointer[thisPoint] == thisNode) break;
-	// 현재노드가 부모 노드의 0번째 인덱스가 아닐 때 
-	broNode = (thisPoint != 0) ? parent->node.indexNode.pointer[thisPoint - 1] : parent->node.indexNode.pointer[thisPoint + 1];
-	// 형재 노드에 서브트리 수를 확인한다.
-	for (i = 0; i < DEGREE; i++)
-		if (broNode->node.indexNode.pointer[i] == NULL) break;
-	// 만약 형재 노드가 현재 노드에게 서브트리 를 줄때 50% 이상이 유지가 안될경우 병합을 한다.
-	if ((DEGREE % 2 == 0 && DEGREE / 2 >= i) || (DEGREE % 2 == 1 && DEGREE / 2 + 1 >= i)) // 병합
-	{
-		// thisPoint == 0 일 때 broNode 와 thisNode 포인터를 교체한다.
-		if (thisPoint == 0)
-		{
-			tempNode = thisNode;
-			thisNode = broNode;
-			broNode = tempNode;
-		}
-		else
-		{
-			thisPoint--;
-		}
-		// 추가할 곳의 포인터를 찾음
-		for (j = 0; j < DEGREE; j++)
-			if (broNode->node.indexNode.pointer[j] == NULL) break;
-		broNode->node.indexNode.key[j - 1] = parent->node.indexNode.key[thisPoint];
-
-		// thisNode 의 포인터를 broNode 노드로 옮김
-		for (i = j, k = 0; i < DEGREE; i++, k++)
-		{
-			broNode->node.indexNode.pointer[i] = thisNode->node.indexNode.pointer[k];
-			broNode->node.indexNode.pointer[i]->parent = broNode;
-		}
-		// thisNode 의 키를 broNode 노드로 옮김
-		for (i = j, k = 0; i < DEGREE - 1; i++, k++)
-			broNode->node.indexNode.key[i] = thisNode->node.indexNode.key[k];
-		broNode->full = FULL;
-		for (i = thisPoint + 1; i < DEGREE - 1; i++)
-			parent->node.indexNode.pointer[i] = parent->node.indexNode.pointer[i + 1];
-
-		parent->node.indexNode.pointer[i] = NULL;
-
-		for (i = thisPoint; i < DEGREE - 2; i++)
-			parent->node.indexNode.key[i] = parent->node.indexNode.key[i + 1];
-		parent->node.indexNode.key[i] = 0;
-		parent->full = 0;
-		free(thisNode);
-		// 서브 트리의 조건이 만족하는지 확인 M차 수일때 최소 50% 이상 서브트리 가지고 있어야함
-		for (i = 0; i < DEGREE; i++)
-			if (parent->node.indexNode.pointer[i] == NULL) break;
-		// 부모노드가 root 이고 서브트리가 하나 밖에 없다면 index 노드가 필요 없음
-		if (parent == root)
-		{
-			if ((i == 1))
-			{
-				root = parent->node.indexNode.pointer[0];
-				root->parent = NULL;
-				free(parent);
-			}
-			return;
-		}
-		// 차수가 짝수이고 50% 이상 유지 이거나 차수가 홀수이고 66% 이상 유지 되면 종료
-		if ((DEGREE % 2 == 0 && DEGREE / 2 <= i) || (DEGREE % 2 == 1 && DEGREE / 2 + 1 <= i)) return;
-		//50%가 유지가 안되면
-		DeleteKey2(parent);
-	}
-	else // 재분배
-	{
-		if (thisPoint == 0)
-		{
-			// 키가 0인 곳을 찾아서 key 값을 설정한다.
-			for (i = 0; i < DEGREE; i++)
-			{
-				if (thisNode->node.indexNode.key[i] == 0)
-				{
-					thisNode->node.indexNode.key[i] = parent->node.indexNode.key[thisPoint];
-					break;
-				}
-			}
-			// broNode 에서 부터 값하나를 받아 온다.
-			thisNode->node.indexNode.pointer[i + 1] = broNode->node.indexNode.pointer[0];
-			thisNode->node.indexNode.pointer[i + 1]->parent = thisNode;
-			parent->node.indexNode.key[thisPoint] = broNode->node.indexNode.key[0];
-			broNode->full = 0;
-			// broNode를 정렬한다.
-			for (j = 0; j < DEGREE - 2; j++)
-			{
-				broNode->node.indexNode.key[j] = broNode->node.indexNode.key[j + 1];
-				broNode->node.indexNode.pointer[j] = broNode->node.indexNode.pointer[j + 1];
-			}
-			broNode->node.indexNode.key[j] = 0;
-			broNode->node.indexNode.pointer[j] = broNode->node.indexNode.pointer[j + 1];
-			broNode->node.indexNode.pointer[j + 1] = NULL;
-		}
-		else
-		{
-			// 형제 노드에 마지막 Key 위치를 확인한다.
-			for (i = 0; i < DEGREE; i++)
-				if (broNode->node.indexNode.pointer[i] == NULL) break;
-			i--;
-			// thisNode를 정렬한다.
-			for (j = DEGREE - 1; j > 0; j--)
-				thisNode->node.indexNode.pointer[j] = thisNode->node.indexNode.pointer[j - 1];
-			for (j = DEGREE - 2; j > 0; j--)
-				thisNode->node.indexNode.key[j] = thisNode->node.indexNode.key[j - 1];
-
-			// broNode에서 값을 받아온다.
-			thisNode->node.indexNode.pointer[0] = broNode->node.indexNode.pointer[i];
-			thisNode->node.indexNode.pointer[0]->parent = thisNode;
-
-			broNode->node.indexNode.pointer[i] = NULL;
-			thisNode->node.indexNode.key[0] = parent->node.indexNode.key[thisPoint - 1];
-
-			parent->node.indexNode.key[thisPoint - 1] = broNode->node.indexNode.key[i - 1];
-
-			broNode->node.indexNode.key[i - 1] = 0;
-			broNode->full = 0;
-		}
-	}
-}
-void DeleteKey1(Node *thisNode)
-{
-	/*
-	DeleteKey1 함수
-	리프 노드의 재분배 및 병합하는 함수
-
-	Node *thisNode : 재 분할 및 병합을 해야하는 노드
-	*/
-	int i, j;
-	int thisPoint;
-	Node *parent;
-	Node *broNode;
-	Node *tempNode;
-	// 현재 노드가 리프노드가 아니라면 중지
-	if (thisNode->type != LEAF) return;
-	// 부모 노드를 설정
-	parent = thisNode->parent;
-	// 현재 리프노드가 루트이면 종료
-	if (parent == NULL) return;
-	// 현재 노드가 부모 노드의 몇번째 서브트리 인지 확인
-	for (thisPoint = 0; thisPoint < DEGREE - 1; thisPoint++)
-		if (parent->node.indexNode.pointer[thisPoint] == thisNode) break;
-	/*
-	현재노드가 부모 노드의
-	0번째 서브트리가 아니면 왼쪽 서브트리와 재분배 및 병합
-	0번째 서브트리면 오른쪽 서브트리와 재분배 및 병합
-	선택된 서브트리를 형제노드라고 한다.
-	*/
-	broNode = (thisPoint != 0) ? parent->node.indexNode.pointer[thisPoint - 1] : parent->node.indexNode.pointer[thisPoint + 1];
-	// 형제노드에 Key 수를 확인한다.
-	for (i = 0; i < DEGREE; i++)
-		if (broNode->node.leafNode.key[i] == 0) break;
-	// 만약 형제노드와 재분재를 했을경우 50% 이상이 안될 때 병합을 한다.
-	if ((DEGREE % 2 == 0 && DEGREE / 2 >= i) || (DEGREE % 2 == 1 && DEGREE / 2 + 1 >= i)) // 병합
-	{
-		/*
-		현재노드가 0번째 서브트리면 broNode와 thisNode 이름을 바꾼다.
-		이는 병합할 때 노드를 하나 버려야되는데 이 때 오른쪽에 있는 노드를 버린다.
-		왼쪽에 있는 노드를 버릴경우 리프노드끼리 연결된 포인터들이 복잡해진다.
-		따라서 이름을 그대로 사용할 경우 오른쪽 노드가 버려지기 때문에 이름을 바꾼다.
-		thisPoint 는 broNode를 가리키는 인덱스로써 사용한다.
-		*/
-		if (thisPoint == 0)
-		{
-			tempNode = thisNode;
-			thisNode = broNode;
-			broNode = tempNode;
-		}
-		else
-			thisPoint--;
-		// thisNode는 버려지기 때문에 다음 리프노드 포인터를 broNode에 설정한다.
-		broNode->node.leafNode.next = thisNode->node.leafNode.next;
-		// thisNode 가 가지고 있는 Key 와 Data 를 broNode 로 옮긴다.
-		for (j = 0; j < DEGREE; j++)
-		{
-			// thisNode가 가지고 있는 정보가 더이상 없으면 빠져나간다.
-			if (thisNode->node.leafNode.key[j] == 0) break;
-			// 현재 가리키고있는 Key 와 Data 를 broNode로 옮긴다.
-			InsertKey1(thisNode->node.leafNode.key[j], thisNode->node.leafNode.data[j], broNode, root);
-		}
-		// 병합을 하게되면 그 노드는 항상 FULL 상태가 된다.
-		broNode->full = FULL;
-		// thisNode의 포인터를 제거하기 위해 부모노드를 정렬한다.
-		for (i = thisPoint + 1; i < DEGREE - 1; i++)
-			parent->node.indexNode.pointer[i] = parent->node.indexNode.pointer[i + 1];
-
-		parent->node.indexNode.pointer[i] = NULL;
-		for (i = thisPoint; i < DEGREE - 2; i++)
-			parent->node.indexNode.key[i] = parent->node.indexNode.key[i + 1];
-		parent->node.indexNode.key[i] = 0;
-		// 제거 되었기 때문에 항상 부모노드는 FULL 상태가 아니게된다.
-		parent->full = 0;
-		// thisNode를 제거한다.
-		free(thisNode);
-		// 인덱스노드가 몇개의 서브트리를 가지고 있는지 확인한다.
-		for (i = 0; i < DEGREE; i++)
-			if (parent->node.indexNode.pointer[i] == NULL) break;
-		// 인덱스노드가 root 이고 서브트리가 하나 밖에 없다면 index 노드가 필요 없음
-		if (parent == root)
-		{
-			if ((i == 1))
-			{
-				root = parent->node.indexNode.pointer[0];
-				root->parent = NULL;
-				free(parent);
-			}
-			return;
-		}
-		// 차수가 짝수이고 50% 이상 유지 이거나 차수가 홀수이고 66% 이상 유지 되면 종료
-		if ((DEGREE % 2 == 0 && DEGREE / 2 <= i) || (DEGREE % 2 == 1 && DEGREE / 2 + 1 <= i)) return;
-		//50%가 유지가 안되면
-		DeleteKey2(parent);
-	}
-	else // 재분배
-	{
-		/*
-		서브트리가 0일때는 오른쪽과 재분배를
-		0이 아닐때는 왼쪽과 재분배를 해야하기 때문에 나눔
-		또한 재분배는 부모 인덱스노드의 서브트리 수를 바꾸지 않기 때문에
-		인덱스노드의 50%조건을 검사하지 않아도 됨
-		*/
-		if (thisPoint == 0)
-		{
-			/*
-			오른쪽에 있는 형제노드에서 값 하나를 가져온다.
-			하나만 가져오는 이유는 삭제 되는 값이 하나씩 이루어지기 때문에
-			50%가 유지가 안된다면 그것은 하나가 부족한 것이다.
-			*/
-			InsertKey1(broNode->node.leafNode.key[0], broNode->node.leafNode.data[0], thisNode, root);
-			/*
-			부모노드의 인덱스를 형제노드에서 가져온 값으로 변경한다.
-			현재노드보다 형제노드가 오른쪽에 있기 때문에
-			무조건 형제노드에서 가져온 값이 크기 때문이다.
-			*/
-			parent->node.indexNode.key[thisPoint] = broNode->node.leafNode.key[0];
-			// 형제노드를 정렬한다.
-			for (i = 0; i < DEGREE - 1; i++)
-			{
-				broNode->node.leafNode.key[i] = broNode->node.leafNode.key[i + 1];
-				broNode->node.leafNode.data[i] = broNode->node.leafNode.data[i + 1];
-			}
-			broNode->node.leafNode.key[i] = 0;
-			broNode->node.leafNode.data[i] = 0;
-			// 하나가 제거되었기 때문에 무조건 FULL 상태가 아니다.
-			broNode->full = 0;
-		}
-		else
-		{
-			// 왼쪽에 형제노드이므로 형제 노드에서 가져올 마지막 값을 검색한다.
-			for (i = 0; i < DEGREE; i++)
-				if (broNode->node.leafNode.key[i] == 0) break;
-			i--;
-			// 검색된 Key 와 Data 를 현재노드에 추가 시킨다.
-			InsertKey1(broNode->node.leafNode.key[i], broNode->node.leafNode.data[i], thisNode, root);
-			// 가져온 Key 와 Data 를 삭제하고 무조건 FULL 상태가 아님
-			broNode->node.leafNode.key[i] = 0;
-			broNode->node.leafNode.data[i] = 0;
-			broNode->full = 0;
-			// 형제노드를 가리키는 인덱스를 바꾸어준다.
-			parent->node.indexNode.key[thisPoint - 1] = broNode->node.leafNode.key[i - 1];
-		}
-	}
-}
-void DeleteKey(float deleteKey)
-{
-	/*
-	DeleteKey 함수
-	트리 에서 현재 키를 삭제 한다.
-	삭제후 그 노드가 50%이상 유지가 되지 않는다면 DeleteKey1 을 실행한다.
-	int deleteKey : 삭제할 키
-	*/
-	int i, j;
-	Node *thisNode;
-	// 존재하지 않는 키라면 삭제 할 수 없음
-	if ((thisNode = FindKey(deleteKey, 1, root)) == NULL) return;
-	// key 값과 같은 Key 가 어느 위치에 있는지 확인
-	for (i = 0; i < DEGREE; i++)
-		if (fabsf(thisNode->node.leafNode.key[i] - deleteKey) < EPS) break;
-	// 그 곳에 있는 Key 를 삭제
-	thisNode->node.leafNode.key[i] = 0;
-	thisNode->node.leafNode.data[i] = 0;
-
-	// 비워 있는 곳을 메꾸기 위해 다시 정렬
-	for (j = i; j < DEGREE - 1; j++)
-	{
-		thisNode->node.leafNode.key[j] = thisNode->node.leafNode.key[j + 1];
-		thisNode->node.leafNode.data[j] = thisNode->node.leafNode.data[j + 1];
-	}
-	thisNode->node.leafNode.key[j] = 0;
-	thisNode->node.leafNode.data[j] = 0;
-	//지웠다면 그 상태는 무조건 FULL 이 아님
-	thisNode->full = 0;
-	// 현재 Key 가 어느정도 있는지 확인
-	for (i = 0; i < DEGREE; i++)
-		if (thisNode->node.leafNode.key[i] == 0) break;
-	// 차수가 짝수이고 50% 이상 유지, 차수가 홀수이고 66% 이상 유지 되면 종료
-	if ((DEGREE % 2 == 0 && DEGREE / 2 <= i) || (DEGREE % 2 == 1 && DEGREE / 2 + 1 <= i)) return;
-	//50%가 유지가 안되면 DeleteKey1 을 이용하여 재분배 및 병합 실행
-	DeleteKey1(thisNode);
-}
-void initBPlusTree()
+void initBPlusTree(Node*& node)
 {
 	/*
 	3 미만으로 하게 되면 삽입과정중
@@ -1496,28 +1176,9 @@ void initBPlusTree()
 	}
 
 	// 루트 생성 및 초기화
-	root = (Node *)malloc(sizeof(Node));
-	memset((char *)root, 0, sizeof(Node));
-	root->type = LEAF;
-}
-void initBPlusTree2()
-{
-	/*
-	3 미만으로 하게 되면 삽입과정중
-	루트가 아닌 인덱스노드의 서브트리 개수가 1개가 될 경우가 있음
-	따라서 조건이 맞지 않음
-	*/
-
-	if (DEGREE < 3)
-	{
-		printf("차수가 3미만이 될수 없습니다.\n");
-		exit(1);
-	}
-
-	// 루트 생성 및 초기화
-	root2 = (Node *)malloc(sizeof(Node));
-	memset((char *)root2, 0, sizeof(Node));
-	root2->type = LEAF;
+	node = (Node *)malloc(sizeof(Node));
+	memset((char *)node, 0, sizeof(Node));
+	node->type = LEAF;
 }
 void addStack(Node *thisNode)
 {
